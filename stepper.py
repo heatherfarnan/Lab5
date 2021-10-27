a6f90d4a9596184f6c86113f333d4b81fa934976
heatherfarnan <91498628+heatherfarnan@users.noreply.github.com>
2021-10-27 19:56:44 +0000

    Initial commit

---
+++ stepper.py
@@ -0,0 +1,68 @@
+from PCF8591 import PCF8591
+
+class Stepper:
+
+  def __init__ (self, address):
+    self.PCF = PCF8591(address)
+
+  def getAngle(self):
+    return self.PCF.read(0) #yuh i need fixing
+
+  def zero(self):
+    return self.PCF.read(1) #yuh i need fixing
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+pins = [18,21,22,23] # controller inputs: in1, in2, in3, in4
+for pin in pins:
+  GPIO.setup(pin, GPIO.OUT, initial=0)
+
+# Define the pin sequence for counter-clockwise motion, noting that
+# two adjacent phases must be actuated together before stepping to
+# a new phase so that the rotor is pulled in the right direction:
+sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+
+state = 0 #current position in stator sequence
+
+def delay_us(tus): # use microseconds to improve time resolution
+  endTime = time.time() + float(tus)/ float(1E6)
+  while time.time() < endTime:
+    pass
+
+def halfsetp(dir):
+  # dir = +/- 1 (ccw / cw)
+  state += dir
+  if state > 7: state = 0
+  elif state < 0: state = 7
+  for pin in range(4):    # 4 pins that need to be energized
+    GPIO.output(pins[pin], sequence[state][pin])
+  delay_us(1000)
+
+
+def moveSteps(steps, dir):
+  #move the actuation sequence a given number of half steps
+  for step in steps:
+    halfstep(dir)
+
+# # Make a full rotation of the output shaft:
+# def loop(dir): # dir = rotation direction (cw or ccw)
+#   for i in range(512): # full revolution (8 cycles/rotation * 64 gear ratio)
+#     for halfstep in range(8): # 8 half-steps per cycle
+#       for pin in range(4):    # 4 pins that need to be energized
+#         GPIO.output(pins[pin], dir[halfstep][pin])
+#       delay_us(1000)
+try:
+  moveSteps(1000, 1)
+except:
+  pass
+GPIO.cleanup() 
+
+#further modify this code
+#convert it into a class
+#use instance methods for that object to get the motor to move the way you want
+#2 new methods: given angle (what's and angle? it's relative to zero. should be able to convert: certain angular resolution associated with a single half step --> how many half steps are needed to go from current angle to desired angle?
+#speaking of which we also need to keep track of current angle
+#input angel is absolute relative to absolute zero. positive angle between 0 and 360
